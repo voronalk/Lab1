8c9b06a59e5742004965523cac32a2b02075038e
Aleksandr Vorona <32168978+CrowAlcoholic@users.noreply.github.com>
2017-09-26 21:39:37 +0300

    Update Classwork1.cpp

--- Classwork1.cpp
+++ Classwork1.cpp
@@ -1,3 +1,4 @@
+#include "stdafx.h"
 #include <iostream>
 #include <math.h>
 
@@ -5,20 +6,20 @@ int main() {
     double pi = 3.14;
     std::cout << pi << std::endl;
     std::cout << 1 << ' ' << 13 << ' ' << 49 << std::endl;
-	char b;
+    char b;
     std::cin >> b;
     std::cout << 1 << b << 13 << b << 49 << std::endl;
     std::cout << "Enter numbers" << std::endl;
-	int k = 0;
-	int n = 0;
-	int h = 0;
+    int k = 0;
+    int n = 0;
+    int h = 0;
     std::cin >> k >> n >> h;
     std::cout << k << ' ' << ' ' << n << ' ' << ' ' << h << std::endl;
     double a = 0;
     std::cout << "Enter a for: x=12a^2 + 7a - 12" << std::endl;
     std::cin >> a;
     std::cout << "Result:" << 12 * a*a + 7 * a - 12 << std::endl;
-	double x = 0;
+    double x = 0;
     std::cout << "Enter x for: y=3x^3 + 4x^2 - 11x + 1" << std::endl;
     std::cin >> x;
     std::cout << "Result:" << 3 * x*x*x + 4 * x*x - 11 * x + 1 << std::endl;
